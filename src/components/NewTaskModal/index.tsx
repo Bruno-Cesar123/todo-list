21a1d8da045619b4d10dfe7d97b711c484ec8a4e
Bruno Cesar <brunocesar12386@gmail.com>
2021-12-29 10:31:25 -0300

    feat: create form modal

--- src/components/NewTaskModal/index.tsx
+++ src/components/NewTaskModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
+import { FiXCircle } from'react-icons/fi';
 
+import styles from './styles.module.scss';
 interface newTaskModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -13,7 +15,32 @@ export function NewTaskModal({ isOpen, onRequestClose }: newTaskModalProps) {
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <h1>testeee</h1>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <FiXCircle size={32} color='#75ADC4' style={{cursor: 'pointer'}} />
+      </button>
+
+      <form className={styles.form}>
+        <h2>Adicionar nova tarefa</h2>
+
+        <input 
+          type="text"
+          placeholder="Título"
+        />
+
+        <input 
+          type="text"
+          placeholder="Duração"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+
+      </form>
     </Modal>
   )
 }
